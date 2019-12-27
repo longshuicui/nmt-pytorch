71f3428fc9de74ddea1e9618ca6f7099eb2255e7
cuilongshui <longshuicui@qq.com>
2019-12-27 17:11:14 +0800

    main

--- code_seq2seq/main.py
+++ code_seq2seq/main.py
@@ -17,6 +17,7 @@ from data_process import load_data, text2tensor
 from model import Encoder,Decoder,Seq2Seq
 
 logging.basicConfig(level=logging.INFO,format="%(asctime)s %(name)s %(levelname)s %(message)s")
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def gen_batch_data(data=None,batch_size=64,src_max_len=50,tar_max_len=60,shuffle=True):
     """
@@ -37,7 +38,9 @@ def gen_batch_data(data=None,batch_size=64,src_max_len=50,tar_max_len=60,shuffle
     batch_data = []
     for pair in data:
         if len(pair[0])<src_max_len:
-            pair[0].extend([0]*(src_max_len-len(pair[0])))
+            temp=[0]*(src_max_len-len(pair[0]))
+            temp.extend(pair[0])
+            pair[0]=temp
         if len(pair[1])<tar_max_len:
             pair[1].extend([0]*(tar_max_len-len(pair[1])))
     for i in range(num):
@@ -45,6 +48,7 @@ def gen_batch_data(data=None,batch_size=64,src_max_len=50,tar_max_len=60,shuffle
         if len(batch_data)==batch_size or i==num-1:
             yield np.array(batch_data)
             batch_data=[]
+
 
 def train(args):
     src_lang, tar_lang, pairs=load_data()
@@ -63,39 +67,72 @@ def train(args):
                     hidden_size=args.hidden_dim,
                     num_layers=args.num_layers,
                     tar_vocab_size=tar_lang.n_words)
-    criterion=nn.NLLLoss()
-    model=Seq2Seq(encoder=encoder,decoder=decoder,criterion=criterion)
+    criterion=nn.CrossEntropyLoss()
+    model=Seq2Seq(encoder=encoder,decoder=decoder,criterion=criterion).to(device)
     opt=torch.optim.Adam(model.parameters(),lr=args.lr)
-    for i, batch_data in enumerate(generator):
-        src_data=torch.tensor(batch_data[:,0].tolist())
-        tar_data=torch.tensor(batch_data[:,1].tolist())
-        loss,outputs=model(src_data,tar_data)
-        logging.info('=' * 10 + "当前迭代%d, 当前损失:%.4f" % (i + 1, loss))
-        print(outputs)
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
+    logging.info('='*10+'Starting training Model...')
+    for iter in range(1,args.iters+1):
+        for i, batch_data in enumerate(generator):
+            src_data=torch.tensor(batch_data[:,0].tolist()).to(device)
+            tar_data=torch.tensor(batch_data[:,1].tolist()).to(device)
 
+            #TODO 模型的损失一直都是0，效果不好，增加注意力机制试一试
+            loss,outputs=model(src_data,tar_data)
+            logging.info('=' * 10 + "【当前迭代%d，批次%d】, 当前损失:%.4f" % (iter,i + 1, loss))
+            for param_group in opt.param_groups:
+                if iter<=2:
+                    param_group['lr']=0.1
+                else:
+                    param_group['lr']=args.lr
+
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+    torch.save({'model_state_dict':model.state_dict(),'src_lang':src_lang,'tar_lang':tar_lang},args.save_path)
+
+
+def test(args):
+    logging.info('=' * 10 + '加载模型')
+    params=torch.load(args.save_path)
+    src_lang,tar_lang=params['src_lang'],params['tar_lang']
+    encoder = Encoder(batch_size=args.batch_size,
+                      seq_len=args.src_max_len,
+                      embedding_size=args.embedding_size,
+                      hidden_size=args.hidden_dim,
+                      num_layers=args.num_layers,
+                      src_vocab_size=src_lang.n_words)
+    decoder = Decoder(batch_size=args.batch_size,
+                      embedding_size=args.embedding_size,
+                      hidden_size=args.hidden_dim,
+                      num_layers=args.num_layers,
+                      tar_vocab_size=tar_lang.n_words)
+    model = Seq2Seq(encoder=encoder, decoder=decoder, flag=False).to(device)
+    model.load_state_dict(params['model_state_dict'])
+    model.eval()
+    print('ok')
 
 
 
 
 def main():
     parser=argparse.ArgumentParser()
-    parser.add_argument('-batch_size',type=int,default=64)
+    parser.add_argument('-batch_size',type=int,default=128)
     parser.add_argument('-src_max_len',type=int,default=50)
-    parser.add_argument('-tar_max_len',type=int,default=70)
+    parser.add_argument('-tar_max_len',type=int,default=61)
     parser.add_argument('-embedding_size',type=int,default=128)
-    parser.add_argument('-hidden_dim',type=int,default=256)
+    parser.add_argument('-hidden_dim',type=int,default=128)
     parser.add_argument('-num_layers',type=int,default=2)
     parser.add_argument('-lr',type=float,default=0.001)
+    parser.add_argument('-iters',type=int,default=10)
+    parser.add_argument('-save_path',type=str,default='./nmt_seq2seq.ckpt')
     parser.add_argument('-teacher_forcing',type=bool,default=True)
     parser.add_argument('-beam_search',type=bool,default=False)
 
     args=parser.parse_args()
 
     train(args)
-
+    test(args)
 
 
 if __name__ == '__main__':
